e047ad6079e053d1b2414db14441f9850be01377
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-30 00:19:12 +0300

    feat: update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/authController';
+import { login, logout, register } from '../controllers/authController';
+import { isAuthenticated } from '../middlewares/authMiddleware';
+import userRouter from './userRouter';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/logout', logout);
+router.use('/users', isAuthenticated, userRouter);
 
 export default router;
