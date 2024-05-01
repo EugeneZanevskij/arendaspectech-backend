942e6540fab077ef75f57ec5aa24b9ed017de52f
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-01 18:16:36 +0300

    feat: update getUser and create getUsers in user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,29 @@
 import { Request, Response } from 'express';
-import { findUserById } from '../model/user';
+import { findUserById, findUsers } from '../model/user';
 
-export const getMe = async(req: Request, res: Response) => {
-  const userId = req.userId;
+export const getUser = async(req: Request, res: Response) => {
+  let userId: number | undefined;
+  const isAdmin = req.user?.isAdmin;
+  if (isAdmin) {
+    userId = +req.params.id;
+  } else {
+    userId = req.user?.id;
+  }
   if (!userId) {
     res.status(400);
-  } 
+  }
   try {
     const user = await findUserById(userId!);
     return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+export const getUsers = async(req: Request, res: Response) => {
+  try {
+    const users = await findUsers();
+    return res.status(200).json(users);
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error' });
   }
