bd204207b6ca9f852645943d34dce4663ee9863c
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-06 17:23:48 +0300

    feat: create EquipmentType controller

---
+++ src/controllers/equipmentTypeController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { findEquipmentTypes, createEquipmentType, updateEquipmentTypeById, deleteEquipmentTypeById } from "../model/equipmentType";
+
+export const getEquipmentTypes = async(req: Request, res: Response) => {
+  try {
+    const equipmentTypes = await findEquipmentTypes();
+    return res.status(200).json(equipmentTypes);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const addEquipmentType = async(req: Request, res: Response) => {
+  const { name } = req.body;
+  try {
+    const equipmentType = await createEquipmentType(name);
+    return res.status(201).json(equipmentType);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const updateEquipmentType = async(req: Request, res: Response) => {
+  const id = +req.params.id;
+  const { name } = req.body;
+  try {
+    const equipmentType = await updateEquipmentTypeById(id, name);
+    return res.status(200).json(equipmentType);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const deleteEquipmentType = async(req: Request, res: Response) => {
+  const id = +req.params.id;
+  try {
+    await deleteEquipmentTypeById(id);
+    return res.status(200).json({ message: 'Equipment type deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
