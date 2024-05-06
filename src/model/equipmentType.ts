cde5d157d6ab50f226f69c0f81299c8cd1f1d592
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-06 17:23:27 +0300

    feat: create EquipmentType model

---
+++ src/model/equipmentType.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function createEquipmentType(name: string) {
+  const equipmentType = await prisma.equipmentType.create({
+    data: {
+      name,
+    },
+  });
+  return equipmentType;
+};
+
+export async function updateEquipmentTypeById(id: number, name: string) {
+  const equipmentType = await prisma.equipmentType.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+    },
+  });
+  return equipmentType;
+};
+
+export async function deleteEquipmentTypeById(id: number) {
+  const equipmentType = await prisma.equipmentType.delete({
+    where: {
+      id,
+    },
+  });
+  return equipmentType;
+};
+
+export async function findEquipmentTypes() {
+  const equipmentTypes = await prisma.equipmentType.findMany();
+  return equipmentTypes;
+}
