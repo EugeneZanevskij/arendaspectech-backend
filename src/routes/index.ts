838db54d4cccdc014108e6b35be8578124469a30
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-07 23:59:56 +0300

    feat: update routes for Equipment and EquipmentToServices

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import { isAuthenticated, isAuthorized } from '../middlewares/authMiddleware';
 import userRouter from './userRouter';
 import servicesRouter from './servicesRouter';
 import equipmentTypeRouter from './equipmentTypeRouter';
+import equipmentRouter from './equipmentRouter';
+import equipmentToServicesRouter from './equipmentToServicesRouter';
+
 
 const router = Router();
 
@@ -13,5 +16,7 @@ router.post('/logout', logout);
 router.use('/users', isAuthenticated, userRouter);
 router.use('/admin/services', isAuthenticated, isAuthorized, servicesRouter);
 router.use('/admin/equipment-type', isAuthenticated, isAuthorized, equipmentTypeRouter);
+router.use('/admin/equipment', equipmentRouter);
+router.use('/admin/equipment-to-services', equipmentToServicesRouter);
 
 export default router;
