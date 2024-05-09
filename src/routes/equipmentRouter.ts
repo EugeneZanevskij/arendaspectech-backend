6ec039511857ea75f74224835f446c1a523baf7e
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-09 22:58:37 +0300

    feat: update Equipment router

--- src/routes/equipmentRouter.ts
+++ src/routes/equipmentRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { addEquipment, deleteEquipment, getEquipments, updateEquipment } from "../controllers/equipmentController";
+import { addEquipment, deleteEquipment, deleteEquipmentToServices, getEquipmentById, getEquipments, updateEquipment } from "../controllers/equipmentController";
 
 const router = Router();
 
 router.get("/", getEquipments);
+router.get("/:id", getEquipmentById);
+router.get("/:id/equipment-to-services", getEquipmentById);
 router.post("/", addEquipment);
 router.put("/:id", updateEquipment);
 router.delete("/:id", deleteEquipment);
+router.delete("/:id/equipment-to-services", deleteEquipmentToServices);
 
 export default router;
