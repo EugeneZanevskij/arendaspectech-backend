f4b4d7483354cb1cc1b38bb20427f76dd912e484
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-09 22:55:50 +0300

    feat: update Equipment controller

--- src/controllers/equipmentController.ts
+++ src/controllers/equipmentController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { findEquipments, createEquipment, updateEquipmentById, deleteEquipmentById, findEquipmentById } from "../model/equipment";
+import { deleteEquipmentToServicesByEquipmentId, findEquipmentToServicesByEquipmentId } from '../model/equipmentToServices';
 
 export const getEquipments = async(req: Request, res: Response) => {
   try {
@@ -20,10 +21,20 @@ export const getEquipmentById = async(req: Request, res: Response) => {
   }
 }
 
-export const addEquipment = async(req: Request, res: Response) => {
-  const { name, description, imagePath, price, equipmentTypeId } = req.body;
+export const getEquipmentToServices = async(req: Request, res: Response) => {
+  const equipmentId = +req.params.id;
   try {
-    const equipment = await createEquipment(name, description, imagePath, +price, equipmentTypeId);
+    const equipmentToServices = await findEquipmentToServicesByEquipmentId(equipmentId);
+    return res.status(200).json(equipmentToServices);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+export const addEquipment = async(req: Request, res: Response) => {
+  const { name, description, imagePath, price, equipmentTypeId, relativePath } = req.body;
+  try {
+    const equipment = await createEquipment(name, description, imagePath, +price, equipmentTypeId, relativePath);
     return res.status(201).json(equipment);
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error' });
@@ -32,9 +43,9 @@ export const addEquipment = async(req: Request, res: Response) => {
 
 export const updateEquipment = async(req: Request, res: Response) => {
   const id = +req.params.id;
-  const { name, description, imagePath, price, equipmentTypeId } = req.body;
+  const { name, description, imagePath, price, equipmentTypeId, relativePath } = req.body;
   try {
-    const equipment = await updateEquipmentById(id, name, description, imagePath, price, equipmentTypeId);
+    const equipment = await updateEquipmentById(id, name, description, imagePath, price, equipmentTypeId, relativePath);
     return res.status(200).json(equipment);
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error' });
@@ -46,6 +57,16 @@ export const deleteEquipment = async(req: Request, res: Response) => {
   try {
     await deleteEquipmentById(id);
     return res.status(200).json({ message: 'Equipment deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+export const deleteEquipmentToServices = async(req: Request, res: Response) => {
+  const equipmentId = +req.params.id;
+  try {
+    await deleteEquipmentToServicesByEquipmentId(equipmentId);
+    return res.status(200).json({ message: 'Equipment to services deleted successfully' });
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error' });
   }
