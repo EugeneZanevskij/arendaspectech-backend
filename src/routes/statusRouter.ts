1872c9aec7b32443d30b53064fb5fbb88b526824
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-17 13:30:45 +0300

    feat: fix statusRouter's middleware

--- src/routes/statusRouter.ts
+++ src/routes/statusRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { addStatus, getStatuses, updateStatus } from "../controllers/statusController";
+import { isAuthorized } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.get("/", getStatuses);
-router.post("/", addStatus);
-router.put("/:id", updateStatus);
+router.post("/", isAuthorized, addStatus);
+router.put("/:id", isAuthorized, updateStatus);
 
 export default router;
