6d792fad7475218874d8d0056dd9fa0e5b5537b8
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-17 13:33:13 +0300

    feat: fix EquipmentToServicesRouter's middleware

--- src/routes/equipmentToServicesRouter.ts
+++ src/routes/equipmentToServicesRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { addEquipmentToServices, deleteEquipmentToServices, getEquipmentToServices, getEquipmentToServicesByEquipmentIdAndServiceId, getEquipmentToServicesById } from "../controllers/equipmentToServicesController";
+import { isAuthorized } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.get("/", getEquipmentToServices);
 router.get("/equipment", getEquipmentToServicesByEquipmentIdAndServiceId);
 router.get("/:id", getEquipmentToServicesById);
-router.post("/", addEquipmentToServices);
-router.delete("/:id", deleteEquipmentToServices);
+router.post("/", isAuthorized, addEquipmentToServices);
+router.delete("/:id", isAuthorized, deleteEquipmentToServices);
 
 export default router;
