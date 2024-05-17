ee125109e4ff7c674cd924580a0fe7d8df8880e9
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-17 13:31:29 +0300

    feat: fix statusRouter's middleware

--- src/routes/servicesRouter.ts
+++ src/routes/servicesRouter.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { addService, deleteService, getService, getServices, updateService } from "../controllers/servicesController";
+import { isAuthorized } from "../middlewares/authMiddleware";
 
 
 const router = Router();
 
 router.get("/", getServices);
 router.get("/:id", getService);
-router.post("/", addService);
-router.put("/:id", updateService);
-router.delete("/:id", deleteService);
+router.post("/", isAuthorized, addService);
+router.put("/:id", isAuthorized, updateService);
+router.delete("/:id", isAuthorized, deleteService);
 
 export default router;
