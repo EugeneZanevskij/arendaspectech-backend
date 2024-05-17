ecadc39cd599ae3f918266a7f63e4dfc4890a73d
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-17 13:34:30 +0300

    feat: fix equipmentRouter's middleware

--- src/routes/equipmentRouter.ts
+++ src/routes/equipmentRouter.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { addEquipment, deleteEquipment, deleteEquipmentToServices, getEquipmentById, getEquipments, updateEquipment } from "../controllers/equipmentController";
+import { isAuthenticated, isAuthorized } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.get("/", getEquipments);
-router.get("/:id", getEquipmentById);
+router.get("/:id", isAuthenticated, getEquipmentById);
 router.get("/:id/equipment-to-services", getEquipmentById);
-router.post("/", addEquipment);
-router.put("/:id", updateEquipment);
-router.delete("/:id", deleteEquipment);
-router.delete("/:id/equipment-to-services", deleteEquipmentToServices);
+router.post("/", isAuthorized, addEquipment);
+router.put("/:id", isAuthorized, updateEquipment);
+router.delete("/:id",isAuthorized, deleteEquipment);
+router.delete("/:id/equipment-to-services",isAuthorized, deleteEquipmentToServices);
 
 export default router;
