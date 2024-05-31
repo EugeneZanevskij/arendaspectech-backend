210524406dc17f0ff4e013a6473f4ba4bbfd87fa
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-05-31 16:00:54 +0300

    feat: add cors origin

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "arendaspectech.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
