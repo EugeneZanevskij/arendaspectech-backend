0b252b9a6dae045c67b0808c6f3433c947c46c22
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-06-03 12:59:45 +0300

    feat: add new cookie parameter

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -16,6 +16,7 @@ export const generateToken = (res: Response, payload: UserBasicInfo) => {
   res.cookie("token", token, {
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "none",
   });
 };
 
